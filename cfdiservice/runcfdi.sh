a3102b7d8f53f73a2a660736b732bc402040b077
adsoftsito <adsoft@live.com.mx>
2021-07-22 23:38:22 +0000

    update cfdi config

--- cfdiservice/runcfdi.sh
+++ cfdiservice/runcfdi.sh
@@ -1,4 +1,10 @@
+# install gunicorn
+sudo yum install python-gunicorn
+
+# start, stop, service
 sudo systemctl status cfdi
 sudo systemctl start cfdi
 sudo systemctl restart cfdi
 
+# reload changes in source
+sudo systemctl daemon-reload
